e2f60f1bafd218a3df5bdafd201305e7293e40d1
Davide Nastri <d.nastri@gmail.com>
2020-08-01 22:38:25 +0200

    feat: Implemented graphqls

---
+++ src/main/java/it/davidenastri/dogsgraphql/DogsgraphqlApplication.java
@@ -0,0 +1,13 @@
+package it.davidenastri.dogsgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsgraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsgraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/davidenastri/dogsgraphql/DogsgraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package it.davidenastri.dogsgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogsgraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
